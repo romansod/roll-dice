af6890f47b8c84c53bc4cffc1bf6dd3533193e8c
romansod <romansod@gmail.com>
2025-01-06 14:57:52 -0500

    [RD-21] feat(internal): Check game state;
    checkSolutionExists->TargetSumExists
    * This recursive check will determine if a solution exists so we can indicate to players when they have 'lost'

--- roll-dice/internal/games/shutthebox.go
+++ roll-dice/internal/games/shutthebox.go
@@ -53,18 +53,17 @@ func (shutTheBox *ShutTheBox) updateGameState(update string) {
 	for _, d := range update {
 		digit_i, _ := strconv.Atoi(string(d))
 
-		SetBitEmpty(&shutTheBox.gameState, digit_i-1)
+		SetBitEmpty(&shutTheBox.gameState, GetValueSlot(digit_i))
 	}
 }
 
 // Visualize the game state
+//
+// Ex: slot 1, 4, 7 are closed:
+//
+//	[_][2][3][_][5][6][_][8][9]
 func (shutTheBox ShutTheBox) printGameState() {
-	gstate := ""
-	for i := 0; i < SizeBox; i++ {
-		gstate += GetSlotForPrint(shutTheBox.gameState, i)
-	}
-
-	fmt.Print(gstate)
+	fmt.Print(AssembleSlotsToDisplay(shutTheBox.gameState))
 }
 
 // Check the win condition: box is shut
@@ -73,6 +72,18 @@ func (shutTheBox ShutTheBox) printGameState() {
 //		bool : true if box is totally shut, false otherwise
 func (shutTheBox ShutTheBox) checkWinCondition() bool {
 	return IsBoxEmpty(shutTheBox.gameState)
+}
+
+// Check whether a solution exists
+//
+//	Params
+//		target int : the target sum to check for among the open slots
+//	Returns
+//		bool : whether there is a solution in the current game state
+//		to satisfy the target
+func (shutTheBox ShutTheBox) checkSolutionExists(target int) bool {
+	gstate := shutTheBox.gameState
+	return TargetSumExists(&gstate, target)
 }
 
 // Check whether provided input is valid to satisfy the target
@@ -167,10 +178,139 @@ func GetSlotForPrint(gstate int, slot int) string {
 	slot_v := EmptySlot
 
 	if IsBitSet(gstate, slot) {
-		slot_v = strconv.Itoa(slot + 1)
+		slot_v = strconv.Itoa(GetSlotValue(slot))
 	}
 
 	return fmt.Sprintf(Slot, slot_v)
+}
+
+// Get the value for the given slot index in the game state
+//
+//	Params
+//		slot int : the slot in the game state who's value we want
+//	Returns
+//		int : the value of the requested slot
+func GetSlotValue(slot int) int {
+	return slot + 1
+}
+
+// Get the slot index for the given value in the game state
+//
+//	Params
+//		value int : the value in the game state who's slot index we want
+//	Returns
+//		int : the slot index of the given value
+func GetValueSlot(value int) int {
+	return value - 1
+}
+
+// Create formatted display for the provided game state
+//
+//	 Ex: gstate(32) -> "[_][_][_][_][_][6][_][_][_]"
+//		Params
+//			gstate int : game state to display
+//		Returns
+//			string : display string
+func AssembleSlotsToDisplay(gstate int) string {
+	gstateslots := ""
+	for i := 0; i < SizeBox; i++ {
+		gstateslots += GetSlotForPrint(gstate, i)
+	}
+
+	return gstateslots
+}
+
+// Helper function to convert displayed game state to internal game state
+//
+// Useful in tests. Example: "[_][_][_][_][_][6][_][_][_]" -> 32
+//
+//	Params
+//		gslots string : formatted slot display for conversion
+//	Returns
+//		int : game state representation
+func ConvertSlotsToGameState(gslots string) int {
+	gstate := 0
+	for i := 0; i < SizeBox; i++ {
+		// Turn each bit on for each open slot
+		gstate |= (ConvertSlotToBit(gslots, i) << i)
+	}
+
+	return gstate
+}
+
+// Helper function to convertdisplayed slot to a bit
+//
+// Ex: [_] -> 0
+// Ex: [4] -> 1
+// Ex: [7] -> 1
+//
+//	Params
+//		gslots string : game state as visual string (9 slots)
+//		slot int      : the slot we want to convert to a bit (off or on)
+//	Returns
+//		int : 0 if [_] and 1 if [1->9]
+func ConvertSlotToBit(gslots string, slot int) int {
+	// [X][.]...
+	// \_\
+	//    \
+	//     slot index 0 in the display format covers gslots[0:3]
+	// We read each slot 3 characters at a time
+	if gslots[slot*3:(slot*3)+3] == fmt.Sprintf(Slot, EmptySlot) {
+		return 0
+	} else {
+		return 1
+	}
+}
+
+// Recursive algorithm to find the first available potential solution in the
+// provided bitset that satisfies the indicated target
+//
+// Terminal State : when target is satisfied by a given slot exactly
+//
+// Recursive Principle : split the target into a low and high that sum to the
+// original target and recurse on those sub targets
+//
+// Game Rules : A slot can only be used once when trying to reach the target
+//
+//	Params
+//		bitset *int : persistent game state used to reach target
+//		target int  : the target sum of open slots in the game state
+//	Returns
+//		bool : true if a solution exists, false otherwise
+func TargetSumExists(bitset *int, target int) bool {
+	// Terminal State -> check if satisfied by single slot
+	if IsBitSet(*bitset, GetValueSlot(target)) {
+		// Consume this bit to satisfy the Game Rules. This
+		// slot can not be used in lateral sub targets
+		SetBitEmpty(bitset, GetValueSlot(target))
+		return true
+	}
+
+	// We copy the incoming game state in case we need to reset due to
+	// an unsolvable sub target
+	orig_bitset := *bitset
+
+	// Create 2 sum programmatically
+	low_v := 1
+	high_v := target - 1
+
+	// Bisecting from the middle means we can avoid symmetric duplication
+	for low_v < high_v {
+		// Recurse over sub targets. If the first sub target does not exist
+		// we can short circuit
+		if TargetSumExists(bitset, low_v) && TargetSumExists(bitset, high_v) {
+			return true
+		} else {
+			// Reset for the next iteration
+			bitset = &orig_bitset
+			// initialize the next permutation
+			low_v++
+			high_v--
+		}
+	}
+
+	// No solution for the current target
+	return false
 }
 
 // Check whether the box is empty, ie all slots closed
